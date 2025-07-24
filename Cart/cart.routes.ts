357e8acef7af3dce1fbe471aa5ff313be55ba262
Javier <J_sedanomar@uncg.edu>
2025-07-24 13:38:29 -0400

    take out redudant /cart in routes

--- Cart/cart.routes.ts
+++ Cart/cart.routes.ts
@@ -3,10 +3,10 @@ const cartRouter = Router();
 import * as cartController from './cart.controller'
 import { authenticateJWT } from '../auth/auth.middleware';
 
-cartRouter.get('/cart', authenticateJWT,cartController.retrieveCartForUser)
-cartRouter.delete('/cart/:productID/remove', authenticateJWT, cartController.removeItemFromCart)
-cartRouter.post('/cart/:productID/add',authenticateJWT,cartController.addItemToCart)
-cartRouter.patch('/cart/item/:productId/increase', authenticateJWT,cartController.increaseQuantityItemFromCart)
-cartRouter.patch('/cart/item/:productId/decrease', authenticateJWT,cartController.decreaseQuantityItemFromCart)
-cartRouter.get('/cart/subtotal',authenticateJWT,cartController.subtotalOfCart)
-cartRouter.post('/cart/placeorder',authenticateJWT,cartController.placeOrderOfCart)
+cartRouter.get('/', authenticateJWT,cartController.retrieveCartForUser)
+cartRouter.delete('/:productID/remove', authenticateJWT, cartController.removeItemFromCart)
+cartRouter.post('/:productID/add',authenticateJWT,cartController.addItemToCart)
+cartRouter.patch('/item/:productId/increase', authenticateJWT,cartController.increaseQuantityItemFromCart)
+cartRouter.patch('/item/:productId/decrease', authenticateJWT,cartController.decreaseQuantityItemFromCart)
+cartRouter.get('/subtotal',authenticateJWT,cartController.subtotalOfCart)
+cartRouter.post('/placeorder',authenticateJWT,cartController.placeOrderOfCart)
