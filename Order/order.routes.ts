ed36ad8dba4761451f4e884be9f58e05706e183d
Javier <J_sedanomar@uncg.edu>
2025-07-24 13:38:07 -0400

    create routes for order actions

---
+++ Order/order.routes.ts
@@ -0,0 +1,10 @@
+import { Router} from 'express'
+const orderRouter = Router();
+import * as orderController from './order.controller'
+import { authenticateJWT } from '../auth/auth.middleware';
+// don't forget to add middleware later to check for admin status for certain routes
+orderRouter.get('/user', authenticateJWT, orderController.retrieveOrders)
+orderRouter.get('/all',authenticateJWT,orderController.retrieveAllOrders)
+orderRouter.get('/unfilled',authenticateJWT,orderController.retrieveAllUnfilled)
+orderRouter.get('/filled',authenticateJWT,orderController.retrieveFilled)
+orderRouter.patch('/mark',authenticateJWT,orderController.markFilledOrUnfilled)
