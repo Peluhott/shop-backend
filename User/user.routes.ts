70540d6a39de8fcf21145a5b8e963db93efdd5a4
Javier <J_sedanomar@uncg.edu>
2025-07-27 10:10:35 -0400

    add entire controller instead of just 1 function

--- User/user.routes.ts
+++ User/user.routes.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import {Request, Response} from 'express'
-import { loginUser } from './user.controller'
+import * as userController from './user.controller'
+import { authenticateJWT } from '../auth/auth.middleware';
 
 const router = express.Router();
 
@@ -8,11 +9,12 @@ const router = express.Router();
 
 router.post("/login", async (req: Request, res: Response) => {
     try {
-      const token = await loginUser(req.body);
+      const token = await userController.loginUser(req.body);
       return res.json({ token });
-    } catch (error) {
+    } catch (error : any) {
       return res.status(401).json({ message: error.message });
     }
   });
 
+router.get('/info',authenticateJWT,userController.returnUserInfo )
   export default router;
