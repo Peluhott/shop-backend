2a63875a476110c8e27cb2921b8a0d1f88111927
Javier <J_sedanomar@uncg.edu>
2025-07-27 10:35:06 -0400

    finish product validator

---
+++ validation/productValidator.ts
@@ -0,0 +1,37 @@
+import {body, param} from 'express-validator'
+
+export const productInfoValidation = [
+    body('name')
+    .isString().withMessage('name must be a string')
+    .notEmpty().withMessage('string can not be empty')
+    .isLength({min:2,max:100}).withMessage('name must be between 2-100 characters')
+    .trim(),
+
+    body('category')
+    .isString().withMessage('category must be a string')
+    .notEmpty().withMessage('string can not be empty')
+    .isLength({min:2,max:100}).withMessage('category must be between 2-100 characters')
+    .trim(),
+
+    body('description')
+    .isString().withMessage('description must be a string')
+    .notEmpty().withMessage('string can not be empty')
+    .isLength({min:2,max:500}).withMessage('description must be between 2-500 characters')
+    .trim(),
+
+    body('price')
+    .isFloat({gt:0})
+    .withMessage('price must be a positive number'),
+
+    body('stock')
+    .isInt({min:0})
+    .withMessage('stock can not be a negative number')
+
+]
+
+export const retrieveProductValidator = [ // use this whenever i get product with id
+    param('id')
+    .isInt({gt:0})
+    .withMessage('id has to be a positive number')
+]
+
