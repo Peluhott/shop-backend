ca3b8627de5c2c40c28d69477081171f7e31873f
Javier <J_sedanomar@uncg.edu>
2025-07-27 23:15:47 -0400

    add validation to product router

--- Product/product.routes.ts
+++ Product/product.routes.ts
@@ -2,11 +2,14 @@ import {Router} from 'express'
 const productRouter = Router()
 import { authenticateJWT } from '../auth/auth.middleware'
 import * as productController from './product.controller'
+import { checkAdmin } from '../middleware/isAdmin'
+import { validateRequest } from '../middleware/validateRequest'
+import { productInfoValidation,  retrieveProductValidator} from '../validation/productValidator'
 
-productRouter.post('/create',authenticateJWT,productController.createProduct)
-productRouter.get('/product/:productId', productController.getProductById)
-productRouter.patch('/update',authenticateJWT,productController.updateProductInfo)
-productRouter.delete('/remove/:productId',authenticateJWT,productController.deleteProduct)
-productRouter.get('/products/all',authenticateJWT,productController.returnAllProducts)
+productRouter.post('/create',authenticateJWT,checkAdmin, productInfoValidation, validateRequest,productController.createProduct)
+productRouter.get('/product/:productId',retrieveProductValidator, validateRequest, productController.getProductById)
+productRouter.patch('/update',authenticateJWT,checkAdmin, productInfoValidation, validateRequest, productController.updateProductInfo)
+productRouter.delete('/remove/:productId',authenticateJWT,checkAdmin, retrieveProductValidator, validateRequest, productController.deleteProduct)
+productRouter.get('/products/all', productController.returnAllProducts) // add pagination
 
 export default productRouter;
