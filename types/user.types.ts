3ca827e01c8f8ee969dc179a45e9fdda28360a31
Javier <J_sedanomar@uncg.edu>
2025-07-28 11:24:07 -0400

    defined userInfo to handle partial data inputs

---
+++ types/user.types.ts
@@ -0,0 +1,12 @@
+
+
+export type UserInfoUpdate = {
+    address?: string;
+    city?: string;
+    state?: string;
+    country?: string;
+    age?: number;
+    gender?: string;
+    email?: string; // optional since you're omitting it later
+  };
+  
