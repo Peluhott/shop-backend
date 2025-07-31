53c3f994fb351950847bf4b019108d8525fd6fb2
Javier <J_sedanomar@uncg.edu>
2025-07-31 13:12:54 -0400

    order test for order routes

---
+++ Order/order.test.ts
@@ -0,0 +1,16 @@
+import request from "supertest"
+import app from '../app'
+import prisma from "../shared/prisma"
+
+describe('GET /order/all', () => {
+    it('returns 401 if not authenticated', async () => {
+        const res = await request(app).get('/order/all')
+        expect(res.status).toBe(401)
+    })
+})
+
+afterAll(async () => {
+    await prisma.$disconnect();   
+                 
+  })
+  
