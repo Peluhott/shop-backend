c7b51fc18dd1a9caf9ebd8c4a88540475ea7b324
Javier <J_sedanomar@uncg.edu>
2025-08-03 00:56:02 -0400

    need to rearrange routes on routers especially if they have :id

--- Product/product.routes.ts
+++ Product/product.routes.ts
@@ -5,11 +5,11 @@ import * as productController from './product.controller'
 import { checkAdmin } from '../middleware/isAdmin'
 import { validateRequest } from '../middleware/validateRequest'
 import { productInfoValidation,  retrieveProductValidator} from '../validation/productValidator'
-
+productRouter.get('/all', productController.returnAllProducts) 
 productRouter.post('/create',authenticateJWT,checkAdmin, productInfoValidation, validateRequest,productController.createProduct)
 productRouter.get('/:productId',retrieveProductValidator, validateRequest, productController.getProductById)
 productRouter.patch('/update',authenticateJWT,checkAdmin, productInfoValidation, validateRequest, productController.updateProductInfo)
 productRouter.delete('/remove/:productId',authenticateJWT,checkAdmin, retrieveProductValidator, validateRequest, productController.deleteProduct)
-productRouter.get('/all', productController.returnAllProducts) // add pagination
-
+// add pagination
+//reorder routes
 export default productRouter;
