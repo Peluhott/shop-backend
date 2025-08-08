cb721847b9de6aec8d1c4e33d920a4908b2b68fa
Javier <J_sedanomar@uncg.edu>
2025-08-08 11:34:44 -0400

    create routes for new functions filter, search, top selling

--- Product/product.routes.ts
+++ Product/product.routes.ts
@@ -5,9 +5,12 @@ import * as productController from './product.controller'
 import { checkAdmin } from '../middleware/isAdmin'
 import { validateRequest } from '../middleware/validateRequest'
 import { productInfoValidation,  retrieveProductValidator} from '../validation/productValidator'
-productRouter.get('/all', productController.returnAllProducts) 
+productRouter.get('/all', productController.returnAllProducts)
+productRouter.get('/filter', productController.getProductsByFilter) 
+productRouter.get('/top', authenticateJWT, checkAdmin, productController.getTopSellingProducts)
+productRouter.get('/search', productController.searchProduct)
 productRouter.post('/create',authenticateJWT,checkAdmin, productInfoValidation, validateRequest,productController.createProduct)
-productRouter.get('/:productId',retrieveProductValidator, validateRequest, productController.getProductById)
+productRouter.get('/id/:productId',retrieveProductValidator, validateRequest, productController.getProductById)
 productRouter.patch('/update',authenticateJWT,checkAdmin, productInfoValidation, validateRequest, productController.updateProductInfo)
 productRouter.delete('/remove/:productId',authenticateJWT,checkAdmin, retrieveProductValidator, validateRequest, productController.deleteProduct)
 // add pagination
