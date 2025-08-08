271ccbd4404b24d6d5dcdf127b1e13f718e1c616
Javier <J_sedanomar@uncg.edu>
2025-08-08 11:35:08 -0400

    create intergration test for get all products

--- Product/product.test.ts
+++ Product/product.test.ts
@@ -1,3 +1,15 @@
 import request from "supertest"
 import app from '../app'
 
+describe('GET /product/all', () => {
+    it('should return all products with 200 status', async () => {
+        const res = await request(app).get('/product/all');
+        expect(res.status).toBe(200)
+        expect(Array.isArray(res.body)).toBe(true)
+
+        if(res.body.length > 0){
+            expect(res.body[0]).toHaveProperty('id')
+            expect(res.body[0]).toHaveProperty('name')
+        }
+    })
+})
