767f679026206e013a01a6d63deb7f8ae9c68f8c
Javier <J_sedanomar@uncg.edu>
2025-08-10 01:24:08 -0400

    create tests for create route of product

--- Product/product.test.ts
+++ Product/product.test.ts
@@ -1,6 +1,14 @@
 import request from "supertest"
 import app from '../app'
 
+
+
+jest.mock('../utils/uploadImage', () => ({
+    uploadProductImage: jest.fn().mockResolvedValue('http.//test/url.jpg')
+}))
+import { uploadProductImage } from "../utils/uploadImage";
+const mockUpload = uploadProductImage as jest.MockedFunction<typeof uploadProductImage>; // to use for counts or errors
+
 describe('GET /product/all', () => {
     it('should return all products with 200 status', async () => {
         const res = await request(app).get('/product/all');
@@ -13,3 +21,42 @@ describe('GET /product/all', () => {
         }
     })
 })
+
+describe('POST /product/create', () => {
+    it('should return a 401', async () => {
+        const res = await request(app).post('/product/create');
+        expect(res.status).toBe(401)
+    })
+
+    it('should return 403 because logged in but not admin', async () => {
+        const login = await request(app)
+        .post('/user/login')
+        .send({username: 'guest', password: 'password'})
+
+        const token = login.body.token;
+        //name , category, description, price, stock
+        const res = await request(app)
+        .post('/product/create')
+        .set('Authorization', `Bearer ${token}`)
+        .send({name:'model1', category:'shirt', description: 'a new green shirt', price: 40, stock: 10})
+
+        expect(res.status).toBe(403);
+    })
+
+    it('should return 201 because its admin and logged in, with valid form data(image mocked)', async () => {
+        
+        const login = await request(app)
+        .post('/user/login')
+        .send({username: 'admin', password: 'password'})
+
+        const token = login.body.token;
+        //name , category, description, price, stock
+        const res = await request(app)
+        .post('/product/create')
+        .set('Authorization', `Bearer ${token}`)
+        .send({name:'model1', category:'shirt', description: 'a new green shirt', price: 40.00, stock: 10})
+
+        expect(res.status).toBe(201);
+    })
+})
+
