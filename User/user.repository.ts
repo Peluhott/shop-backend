eac606b5f6c3ddfa1ee0de90546276e31018fa1d
Javier <J_sedanomar@uncg.edu>
2025-08-22 10:04:49 -0400

    finish user queries

--- User/user.repository.ts
+++ User/user.repository.ts
@@ -1,77 +1,84 @@
 import prisma from '../shared/prisma'
 import { UserInfoUpdate } from '../types/user.types';
 
-export async function insertUser(username: string, password: string,) { // change this to transaction to also create a userinfo row in table
-    return await prisma.user.create({ // took out name from arguments
-        data: {
-            
-            username,
-            password,
-            is_admin: false
-            
-        }
+// Transaction: create user and userInfo together
+export async function insertUser(username: string, password: string) {
+    return await prisma.$transaction(async (tx) => {
+        const user = await tx.user.create({
+            data: {
+                username,
+                password,
+                is_admin: false
+            }
+        });
+        await tx.userInfo.create({
+            data: {
+                userId: user.id,
+                email: '', // Set default or require email in args
+            }
+        });
+        return user;
     });
 }
 
-export async function getUserById(id : number) {
+export async function getUserById(id: number) {
     return await prisma.user.findUnique({
-        where: {id}
+        where: { id }
     });
 }
 
-export async function getUserByUsername(username:string) {
+export async function getUserByUsername(username: string) {
     return await prisma.user.findUnique({
-        where: {username}
+        where: { username }
     });
 }
-
-//export async function updateUserInformation(params:type) {
-    // figure this one out later
-//}
 
 export async function deleteUser(id: number) {
-    await prisma.user.delete({
-        where: {id}
-    })
+    return await prisma.user.delete({
+        where: { id }
+    });
 }
 
 export async function isUsernameTaken(username: string) {
     const user = await prisma.user.findUnique({
-        where: {username}
+        where: { username }
     });
-    return !!user
+    return !!user;
 }
 
 export async function getAllUsers() {
-    const results = await prisma.user.findMany()
-    return results;
+    return await prisma.user.findMany();
 }
 
 export async function promoteUser(id: number) {
-    await prisma.user.update({
-        where:{id},
-        data: {is_admin : true}
+    return await prisma.user.update({
+        where: { id },
+        data: { is_admin: true }
     });
 }
 
-export async function getUserInfo(id: number){
-    return await prisma.userInfo.findUnique({ // might return null in some fields
-        where:{userId:id} 
-    })
+export async function getUserInfo(id: number) {
+    return await prisma.userInfo.findUnique({
+        where: { userId: id }
+    });
 }
 
-export async function updateUserInfo(userid: number, email: string, data: Partial<Omit<UserInfoUpdate, 'email'>>){
-    await prisma.userInfo.update({
-        where:{userId:userid},
+export async function updateUserInfo(
+    userid: number,
+    email: string,
+    data: Partial<Omit<UserInfoUpdate, 'email'>>
+) {
+    return await prisma.userInfo.update({
+        where: { userId: userid },
         data: {
             email,
-            ...(data.address && {phone: data.address}),
-            ...(data.city && {city: data.city}),
-            ...(data.state && {state: data.state}),
-            ...(data.country && {state: data.country}),
-            ...(data.age !== undefined && {age: data.age}),
-            ...(data.gender && {gender: data.gender})
+            ...(data.address && { address: data.address }),
+            ...(data.city && { city: data.city }),
+            ...(data.state && { state: data.state }),
+            ...(data.country && { country: data.country }),
+            ...(data.zipcode && { zipcode: data.zipcode }),
+            ...(data.age !== undefined && { age: data.age }),
+            ...(data.gender && { gender: data.gender })
         }
-
     });
 }
