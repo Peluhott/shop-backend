dbd3208e50a036fe39357a7aa0693ca4da84517d
Javier <J_sedanomar@uncg.edu>
2025-08-24 00:37:49 -0400

    added function to update user info

--- User/user.service.ts
+++ User/user.service.ts
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt'
 import * as userQueries from './user.repository'
 import jwt from 'jsonwebtoken'
+import { UserInfoUpdate } from '../types/user.types'
 
 export async function loginUserService(username: string, password: string) {
     const user = await userQueries.getUserByUsername(username)
@@ -22,12 +23,32 @@ export async function loginUserService(username: string, password: string) {
     return token
 }
 
-export async function createUserService(name: string, username: string, password: string) {
+export async function createUserService(email: string, username: string, password: string) {
     const hashedPassword = await bcrypt.hash(password, 10)
-    return await userQueries.insertUser(username, hashedPassword)
-    // You can add logic to create a cart for the user here if needed
+    return await userQueries.insertUser(username, hashedPassword, email)
+    // make sure cart and user info are created to user as well
 }
 
 export async function getUserInfoService(userId: number) {
     return await userQueries.getUserInfo(userId)
+}
+
+export async function upsertUserInfoService(userId: number, info: Partial<Omit<UserInfoUpdate, 'userId'>>) {
+    const existing = await userQueries.getUserInfo(userId)
+    if (existing) {
+        // Update existing info
+        return await userQueries.updateUserInfo(
+            userId,
+            
+            info
+        )
+    } else {
+        // Create new info
+        return await userQueries.createUserInfo(userId, info)
+    }
+}
+
+export async function userInfoExistsService(userId: number) {
+    const info = await userQueries.getUserInfo(userId)
+    return !!info
 }
