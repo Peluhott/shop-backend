f5821b093dc2ac09e821e3ecec2e907ed10dbe02
Javier <J_sedanomar@uncg.edu>
2025-08-24 00:37:31 -0400

    added routes for create user, get user info, update user info, and made login route cleaner

--- User/user.routes.ts
+++ User/user.routes.ts
@@ -1,20 +1,17 @@
 import express from 'express'
-import {Request, Response} from 'express'
 import * as userController from './user.controller'
 import { authenticateJWT } from '../auth/auth.middleware';
 
 const router = express.Router();
 
+router.post("/login", userController.loginUser);
+
+router.get('/info', authenticateJWT, userController.returnUserInfo);
+
+router.post('/create', userController.createUser);
+
+router.post('/info/upsert', authenticateJWT, userController.upsertUserInfo);
 
 
-router.post("/login", async (req: Request, res: Response) => {
-    try {
-      const token = await userController.loginUser(req.body);
-      return res.json({ token });
-    } catch (error : any) {
-      return res.status(401).json({ message: error.message });
-    }
-  });
 
-router.get('/info',authenticateJWT,userController.returnUserInfo )
-  export default router;
+export default router;
