26e5375642b828072f13c06a0a0330bfe93f4e21
Javier <J_sedanomar@uncg.edu>
2025-08-29 18:09:49 -0400

    retrieve status from openai reponse

--- ai/ai.parser.ts
+++ ai/ai.parser.ts
@@ -1,5 +1,6 @@
 export async function parser(response: any) {
     const functionName = response.output[0].name
-    const status = response.output[0].status || "none"
+    const args = JSON.parse(response.output[0].arguments);
+    const status = args.status || "no status"
     return {functionName, status}
 }
