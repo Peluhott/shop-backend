f0a350a20e7897537e8f75003149d429c59c504f
peluhott <j_sedanomar@uncg.edu>
2025-09-06 12:31:29 -0400

    refactor: update product routes and validation to use query parameters for filtering and searching

--- Product/product.routes.ts
+++ Product/product.routes.ts
@@ -4,15 +4,31 @@ import { authenticateJWT } from '../auth/auth.middleware'
 import * as productController from './product.controller'
 import { checkAdmin } from '../middleware/isAdmin'
 import { validateRequest } from '../middleware/validateRequest'
-import { productInfoValidation,  retrieveProductValidator} from '../validation/productValidator'
+import { 
+    productInfoValidation,  
+    retrieveProductValidator,
+    updateProductValidation,
+    filterProductValidator,
+    searchProductValidator,
+    topSellingProductValidator
+} from '../validation/productValidator'
+
 productRouter.get('/all', productController.returnAllProducts)
-productRouter.get('/filter', productController.getProductsByFilter) 
-productRouter.get('/top', authenticateJWT, checkAdmin, productController.getTopSellingProducts)
-productRouter.get('/search', productController.searchProduct)
-productRouter.post('/create',authenticateJWT,checkAdmin, productInfoValidation, validateRequest,productController.createProduct)
-productRouter.get('/id/:productId',retrieveProductValidator, validateRequest, productController.getProductById)
-productRouter.patch('/update/:id',authenticateJWT,checkAdmin, productInfoValidation, validateRequest, productController.updateProductInfo)
-productRouter.delete('/remove/:productId',authenticateJWT,checkAdmin, retrieveProductValidator, validateRequest, productController.deleteProduct)
+
+productRouter.get('/filter', filterProductValidator, validateRequest, productController.getProductsByFilter)
+
+productRouter.get('/top', authenticateJWT, checkAdmin, topSellingProductValidator, validateRequest, productController.getTopSellingProducts)
+
+productRouter.get('/search', searchProductValidator, validateRequest, productController.searchProduct)
+
+productRouter.post('/create', authenticateJWT, checkAdmin, productInfoValidation, validateRequest, productController.createProduct)
+
+productRouter.get('/id/:productId', retrieveProductValidator, validateRequest, productController.getProductById)
+
+productRouter.patch('/update/:id', authenticateJWT, checkAdmin, updateProductValidation, validateRequest, productController.updateProductInfo)
+
+productRouter.delete('/remove/:productId', authenticateJWT, checkAdmin, retrieveProductValidator, validateRequest, productController.deleteProduct)
+
 // add pagination
 //reorder routes
 export default productRouter;
