ad474ddf50d599612a94da3929a52c268e98f6a1
Javier <J_sedanomar@uncg.edu>
2025-09-29 07:03:01 -0400

    fix: update default server port to 4000 for local development

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
 import app from './app'
 
 // Use Render's provided port or default to 5000 for local development
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`API on ${port}`);
 });
