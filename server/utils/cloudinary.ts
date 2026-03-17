f04603489e80be82e214558514b22efc4325d15b
Javier <J_sedanomar@uncg.edu>
2026-03-17 00:26:54 -0400

    style: format cloudinary configuration for consistency

--- server/utils/cloudinary.ts
+++ server/utils/cloudinary.ts
@@ -1,15 +1,13 @@
-import {v2 as cloudinary} from 'cloudinary'
+import { v2 as cloudinary } from "cloudinary";
 
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_APIKEY,
-    api_secret: process.env.CLOUDINARY_SECRET
-    
-
-})
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_APIKEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 
 export default cloudinary;
